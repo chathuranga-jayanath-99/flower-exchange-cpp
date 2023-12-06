9e4c0f6426ae2696b6339862451e8087dbb14c2c
Ravindu <ravindumadawalagama@gmail.com>
2023-12-06 23:49:17 +0530

    Singleton for exchange application

--- src/ExchangeApplication.cpp
+++ src/ExchangeApplication.cpp
@@ -3,7 +3,12 @@
 
 class ExchangeApplication {
     private: 
-
+        static ExchangeApplication& get() { return ex_app;}
     public: 
-        
+        ExchangeApplication() {};
+        ~ExchangeApplication() {};
+
+        static ExchangeApplication ex_app;        
 }
+
+ExchangeApplication ExchangeApplication::ex_app;
