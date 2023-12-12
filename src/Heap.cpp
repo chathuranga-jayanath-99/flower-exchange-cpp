b4b8bfef11cd3eba8031e26b74c25a5cf7c40a30
chaturannga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-12 21:37:28 +0530

    remove main method

--- src/Heap.cpp
+++ src/Heap.cpp
@@ -75,33 +75,3 @@ void MinHeap::update_top_item_quantity(int newQuantity) {
     heap[0].setQty(newQuantity);
 }
 
-int main() {
-    vector<OrderBookItem> v;
-    
-    OrderBookItem o1("a1", 100, 50);
-    OrderBookItem o2("a2", 100, 50);
-    OrderBookItem o3("a3", 100, 50);
-
-    MaxHeap m(v);
-    m.insert(o1);
-    m.insert(o2);
-    m.insert(o3);
-
-    OrderBookItem pt = m.peek_top();
-    cout << pt.getOrderId() << endl;
-    cout << pt.getQty() << endl;
-
-
-    m.update_top_item_quantity(200);
-    pt = m.peek_top();
-    cout << pt.getOrderId() << endl;
-    cout << pt.getQty() << endl;
-
-    // pt = m.pop_top();
-    // pt = m.peek_top();
-    // cout << pt.getOrderId() << endl;
-
-    // pt = m.pop_top();
-    // cout << pt.getOrderId() << endl;
-    return 0;
-};
